bb8a960d08fc0ffc7c181cb6594475ebbb3621ce
Admin <kapusta.mail@gmail.com>
2021-02-15 14:36:42 +0100

    added javascript for accordion in html

---
+++ Knizka_Project/src/main/assets/js/tdUtils.js
@@ -0,0 +1,20 @@
+
+function assignAccordions() {
+	
+   var acc = document.getElementsByClassName("tdAccordion");
+   var i;
+
+   for (i = 0; i < acc.length; i++) {
+     acc[i].addEventListener("click", function() {
+    
+        this.classList.toggle("active");
+    
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+     });
+   }
+ }
