e8c4ec587ead0acf0ff90c595e4455787bdf3a72
Admin <kapusta.mail@gmail.com>
2022-02-27 22:18:38 +0100

    finalized version 2.1.1 - updated texts

---
+++ Knizka_Project/src/main/assets/sql/upgrade-5.sql
@@ -0,0 +1,12 @@
+
+-- insert tags
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000277); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#prijímanie, #omša' WHERE uf_handle_id_ref = 11000277;
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000278); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#stolovanie' WHERE uf_handle_id_ref = 11000278;
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000279); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#stolovanie, #požehnanie' WHERE uf_handle_id_ref = 11000279;
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000280); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#stolovanie' WHERE uf_handle_id_ref = 11000280;
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000281); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#k svätému mužovi, #rodina' WHERE uf_handle_id_ref = 11000281;
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000282); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#chorí umierajúci' WHERE uf_handle_id_ref = 11000282;
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000283); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#k svätému mužovi, #rodina' WHERE uf_handle_id_ref = 11000283;
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000284); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#chorí umierajúci' WHERE uf_handle_id_ref = 11000284;
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000285); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#k svätému mužovi' WHERE uf_handle_id_ref = 11000285;
+INSERT OR IGNORE INTO user_flags (uf_handle_id_ref) VALUES (11000286); UPDATE user_flags SET uf_tag_list = IFNULL(uf_tag_list, '') || ', ' || '#vnútorné uzdravenie' WHERE uf_handle_id_ref = 11000286;
